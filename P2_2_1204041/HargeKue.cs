3feca1eb1efbba3cb599e7ed465b232f6d6600aa
BryanFlava <bray.gaul99@gmail.com>
2021-11-18 23:13:35 +0700

    first commit

---
+++ P2_2_1204041/HargeKue.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2_2_1204041
+{
+    class HargeKue
+    {
+        public string namaKue;
+        public string rasaKue;
+        public int hargaPCS;
+        public int hargaKue;
+
+        // method yang tidak mengembalikan nilai
+        public void TampilkanNamaKue()
+        {
+            Console.WriteLine("Nama Kue : " + this.namaKue);
+        }
+
+        public void TampilkanJenisRasa()
+        {
+            Console.WriteLine("Rasa Kue : " + this.rasaKue);
+        }
+
+        // method yang mengembalikan nilai
+        public void DapatkanJumlahHargaKue()
+        {
+            Console.WriteLine("Harga Kue :" + this.hargaKue);
+        }
+    }
+}
---
+++ P2_2_1204041/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2_2_1204041
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+            {
+                HargeKue kue = new HargeKue();
+
+            kue.namaKue = "Bolu Lumer";
+            kue.rasaKue = "Rasa Red Valvet";
+            kue.hargaKue = 100000;
+
+            kue.TampilkanNamaKue();
+            Console.WriteLine("Rasa Kue : " + kue.rasaKue);
+            kue.DapatkanJumlahHargaKue();
+            }
+        }
+    }
